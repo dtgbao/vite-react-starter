c51b846ec4343973937419f4682b63b46e9035d8
baodang <kennydang2105@gmail.com>
2023-04-24 16:05:44 +0700

    🐛 Fix createTheme error

--- src/layouts/LayoutDefault/LayoutDefault.tsx
+++ src/layouts/LayoutDefault/LayoutDefault.tsx
@@ -4,9 +4,9 @@ import { ReactNode } from "react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-   palette: {
-      primary: {},
-   },
+   // palette: {
+   //    primary: {},
+   // },
 });
 
 function LayoutDefault({ children }: { children: ReactNode }) {
