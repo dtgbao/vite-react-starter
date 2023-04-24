e6af21fc3d7d0c41240fec049dcf5e6d4a6f4538
baodang <kennydang2105@gmail.com>
2023-04-24 16:06:09 +0700

    💄 Add <CssBaseline/> for project

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,32 @@ import { Route, Routes } from "react-router-dom";
 import { routes } from "./routes";
 import { Suspense } from "react";
 import LayoutDefault from "./layouts/LayoutDefault";
+import { CssBaseline } from "@mui/material";
 
 function App() {
    return (
-      <LayoutDefault>
-         <Routes>
-            {routes.map((route) => {
-               const Component = route.element;
-               return (
-                  <Route
-                     key={route.path || "/"}
-                     index={route.index}
-                     path={route.path}
-                     element={
-                        <Suspense fallback={<>Loading...</>}>
-                           <Component />
-                        </Suspense>
-                     }
-                  />
-               );
-            })}
-         </Routes>
-      </LayoutDefault>
+      <>
+         <CssBaseline />
+         <LayoutDefault>
+            <Routes>
+               {routes.map((route) => {
+                  const Component = route.element;
+                  return (
+                     <Route
+                        key={route.path || "/"}
+                        index={route.index}
+                        path={route.path}
+                        element={
+                           <Suspense fallback={<>Loading...</>}>
+                              <Component />
+                           </Suspense>
+                        }
+                     />
+                  );
+               })}
+            </Routes>
+         </LayoutDefault>
+      </>
    );
 }
 
